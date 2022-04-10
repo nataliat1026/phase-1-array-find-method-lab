5e83b989664eaa6840c19c822fff50a07d94fe29
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-10 18:37:52 -0400

    commit tree 4f19dd12ae6a5ad64461c8e87edefea33c397dae

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+const superbowlWin = (record) => {
+    let findWin = record.find(
+        (obj) => {
+            if (obj.result === `W`) {
+                return obj.year;
+            }
+        }
+    )
+    if (findWin === undefined) {
+        return findWin;
+    } else {
+        return findWin.year;
+    }
+}
